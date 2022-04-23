c0c4458ff67d762bec2a0974ac72bcceb9b676e6
a a <t.mirzeyev@mail.com>
2022-04-23 14:04:32 +0300

    finish

--- 13.py
+++ 13.py
@@ -1,5 +1,6 @@
-#Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
-#Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
-n=int(input("write number n"))
-for y in range(1,n+1):
-    print(f"{y}:{3*y+1}")
+#Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+str1='''A text file (sometimes spelled textfile; an old alternative name is flatfile) is a kind of computer file that is structured as a sequence of lines of electronic text. A text file exists stored as data within a computer file system. In operating systems such as CP/M and MS-DOS, where the operating system does not keep track of the file size in bytes, the end of a text file is denoted by placing one or more special characters, known as an end-of-file marker, as padding after the last line in a text file. On modern operating systems such as Microsoft Windows and Unix-like systems, text files do not contain any special EOF character, because file systems on those operating systems keep track of the file size in bytes. Most text files need to have end-of-line delimiters, which are done in a few different ways depending on operating system. Some operating systems with record-orientated file systems may not use new line delimiters and will primarily store text files with lines separated as fixed or variable length records.
+
+"Text file" refers to a type of container, while plain text refers to a type of content.'''
+str2="is"
+print(str1.count(str2))
